d1f60913b6f0a5dc9676b1d89488d629834af738
titangene <titangene.tw@gmail.com>
2022-01-07 18:14:02 +0800

    refactor: move pinia to "plugins" folder

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 
 import './styles/main.scss';
 
 import App from './App.vue';
+import pinia from './plugins/pinia';
 import router from './plugins/vue-router';
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 
 app.mount('#app');
